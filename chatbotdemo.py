1f4914c03d2054df728e7e02b96b0e60352657ac
Luisa Heise <luisaheise3277@gmail.com>
2020-12-22 20:33:22 -0300

    add requirements.txt and notebook

--- chatbotdemo.py
+++ chatbotdemo.py
@@ -8,14 +8,14 @@ import nltk
 from nltk import word_tokenize
 import os
 
-def main():
+def main(nquest):
     # https://drive.google.com/file/d/1d7zI16_UnKQIpTIr_hcg-p6P_5mziA_n/view?usp=sharing
     FILENAME = "q_and_a.xlsx"
     #os.system(f"wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1d7zI16_UnKQIpTIr_hcg-p6P_5mziA_n' -O {FILENAME}") 
 
     qe = de.QuestionEmbeddings(question_path=FILENAME, greeting="Olá, tudo bem?", no_answer="Em breve, alguns dos nossos membros te responderá!")
 
-    while True:
+    for i in range(nquest):
         question = input("Faça uma pergunta: \n")
         ans = qe.get_response(question)
         print(ans)
