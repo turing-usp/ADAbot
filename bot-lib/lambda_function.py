29a6e58eb6b18d8301b108274e7a55f9122a4b88
Luísa Mendes Heise <43735789+luisaheise@users.noreply.github.com>
2021-03-03 18:39:53 -0300

    Update lambda_function.py

--- bot-lib/lambda_function.py
+++ bot-lib/lambda_function.py
@@ -29,12 +29,6 @@ def respond(sender, message):
     response = get_bot_response(message)
     send_message(sender, response)
 
-
-def is_user_message(message):
-    return (message.get('message') and
-            message['message'].get('text') and
-            not message['message'].get("is_echo"))
-
 def send_message(recipient_id, text):
     payload = {
         'messaging_type': 'RESPONSE'
@@ -70,7 +64,7 @@ def lambda_handler(event, context):
             messaging_event = event_entry0['messaging'][0]
             msg_txt   = messaging_event['message']['text']
             sender_id = messaging_event['sender']['id']
-            send_message(sender_id, msg_txt)
+            respond(sender_id, msg_txt)
 
 
 
