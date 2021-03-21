48b7b36ee8b89298a2a70bdd77daca49300c12f7
Noel <noel.eliezer@pm.me>
2021-03-21 19:22:17 -0300

    update formatting

--- bot-lib/aux/download_nlp_assets.py
+++ bot-lib/aux/download_nlp_assets.py
@@ -1,20 +1,23 @@
 from transformers import AutoModel, AutoTokenizer
 
+
 def get_model(model):
-  """Loads model from Hugginface model hub"""
-  try:
-    model = AutoModel.from_pretrained(model)
-    model.save_pretrained('../model')
-  except Exception as e:
-    raise(e)
+    """Loads model from Hugginface model hub"""
+    try:
+        model = AutoModel.from_pretrained(model)
+        model.save_pretrained('../model')
+    except Exception as e:
+        raise (e)
+
 
 def get_tokenizer(tokenizer):
-  """Loads tokenizer from Hugginface model hub"""
-  try:
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer, do_lower_case=False)
-    tokenizer.save_pretrained('../model')
-  except Exception as e:
-    raise(e)
+    """Loads tokenizer from Hugginface model hub"""
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer, do_lower_case=False)
+        tokenizer.save_pretrained('../model')
+    except Exception as e:
+        raise (e)
+
 
 get_model('neuralmind/bert-base-portuguese-cased')
 get_tokenizer('neuralmind/bert-base-portuguese-cased')
--- bot-lib/aux/dynamobd_handler.py
+++ bot-lib/aux/dynamobd_handler.py
@@ -1,8 +1,8 @@
-
 import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key
 import uuid
+
 
 class DynamodbHandler:
     def __init__(self, DYNAMODB_NLP_BOT_TURING, MESSAGE_TABLE, RATING_TABLE):
@@ -10,14 +10,14 @@ class DynamodbHandler:
         self.messages_table = self.dynamodb.Table(MESSAGE_TABLE)
         self.rating_table = self.dynamodb.Table(RATING_TABLE)
 
-    def put_message(self,user_id, time, message_txt, bot_given_response):
+    def put_message(self, user_id, time, message_txt, bot_given_response):
         response = self.messages_table.put_item(
-        Item={
+            Item={
                 'interaction_id': str(uuid.uuid4()),
                 'user_id': user_id,
                 'time': time,
                 'message_txt': message_txt,
-                'bot_given_response': bot_given_response
+                'bot_given_response': bot_given_response,
             }
         )
         return response
@@ -26,29 +26,28 @@ class DynamodbHandler:
         try:
             rating = float(rating)
             response = self.rating_table.put_item(
-            Item={
-                'interaction_id': str(uuid.uuid4()),
-                'user_id': user_id,
-                'time': time,
-                'message_user': message_user,
-                'bot_given_response': bot_given_response,
-                'rating': rating
-            })
+                Item={
+                    'interaction_id': str(uuid.uuid4()),
+                    'user_id': user_id,
+                    'time': time,
+                    'message_user': message_user,
+                    'bot_given_response': bot_given_response,
+                    'rating': rating,
+                }
+            )
             return response
-        except:
+        except Exception:
             return None
-            
+
     def get_last_interaction(self, user_id):
         try:
-            response = self.messages_table.query(
-                KeyConditionExpression=Key('user_id').eq(user_id)
-            )
+            response = self.messages_table.query(KeyConditionExpression=Key('user_id').eq(user_id))
             items = response['Items']
             last_time = -999
             last_interaction = ""
             last_bot_response = ""
             for item in items:
-                if item['time']>last_time:
+                if item['time'] > last_time:
                     last_time = item['item']
                     last_interaction = item['message_txt']
                     last_bot_response = item['bot_given_response']
